e9c0b4dbc11740d331e643884aa9bdf077b47a21
Andrey Efimov <efiand@ya.ru>
2022-03-11 18:37:30 +0300

    1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
-# pineglade-w3c
-Online node W3C validator with fallback to offline version
+# pineglade-w3c [![npm version](https://img.shields.io/npm/v/pineglade-w3c.svg)](https://www.npmjs.com/package/pineglade-w3c)
+
+Онлайн-валидатор W3C с резервным оффлайн-валидатором.
+
+
+## Установка
+
+`npm i -E pineglade-w3c`
+
+
+## Использование в произвольном коде
+
+```js
+const { validateHtml } = require('pineglade-w3c');
+
+// Где-то внутри middleware, получившей HTML-код:
+if (process.env.NODE_ENV === 'development') {
+  const validationMessage = await validateHtml({ htmlCode });
+  console.log(validationMessage);
+}
+
+```
+
+`validateHtml` принимает объект с параметрами:
+1. `htmlCode` - проверяемый HTML-код.
+2. `sourceName` - источник кода, отображаемый перед выводом номеров строки и символа. По умолчанию - пустая строка.
+3. `htmlvalidateOptions` - опции [резервного валидатора](https://www.npmjs.com/package/html-validate). По умолчанию - содержимое `.htmlvalidate.json` данного пакета.
+
+## Использование в posthtml.config.js
+
+
+```js
+const { getPosthtmlW3c } = require('pineglade-w3c');
+
+module.exports = () => ({
+  plugins: [
+    // other plugins
+    getPosthtmlW3c()
+  ]
+});
+
+```
+
+`getPosthtmlW3c` может принимать объект с необязательными параметрами:
+
+1. `getSourceName` - коллбэк для генерации имени источника на основе posthtml-дерева. По умолчанию - `(tree) => tree.options.from` .
+2. `log` - коллбэк-логгер. По умолчанию - `console.log`.
+3. `htmlvalidateOptions` - Опции [резервного валидатора](https://www.npmjs.com/package/html-validate). По умолчанию - содержимое `.htmlvalidate.json` данного пакета.
---
+++ index.js
@@ -0,0 +1,94 @@
+const { HtmlValidate } = require('html-validate');
+const chalk = require('chalk');
+const fetch = require('node-fetch');
+const render = require('posthtml-render');
+const DEFAULT_OPTIONS = require('./.htmlvalidate.json');
+
+const validateInstance = new HtmlValidate();
+const SeverityLevel = {
+	error: 2,
+	info: 1
+};
+const Severity = {
+	1: {
+		logOutput: chalk.yellow.bold,
+		title: 'WARNING'
+	},
+	2: {
+		logOutput: chalk.red.bold,
+		title: 'ERROR'
+	}
+};
+const W3C_TIMEOUT = 1000;
+
+const validateHtml = async ({
+	htmlCode,
+	sourceName = '',
+	htmlvalidateOptions = DEFAULT_OPTIONS
+}) => {
+	let output = '';
+	const sourceStr = sourceName ? `${sourceName} ` : '';
+
+	const controller = new AbortController();
+	setTimeout(() => {
+		controller.abort();
+	}, W3C_TIMEOUT);
+
+	try {
+		// Онлайн-валидатор HTML
+
+		const validRes = await fetch('https://validator.nu/?out=json', {
+			body: htmlCode,
+			headers: { 'Content-Type': 'text/html' },
+			method: 'POST',
+			signal: controller.signal
+		});
+		const { messages = [] } = await validRes.json();
+		messages.forEach(({ extract, firstColumn, lastLine, message, type }) => {
+			const { logOutput, title } = Severity[SeverityLevel[type]];
+			const prefix = `\n[${chalk.cyan('Validate HTML Online')}] ${sourceStr}(${lastLine}:${firstColumn + 1})`;
+			const selectorMsg = ` ${chalk.cyan(extract)}`;
+
+			output += `${prefix}${selectorMsg}:\n${logOutput.underline(title)}: ${logOutput(message)}\n`;
+		});
+	} catch (err) {
+		// Оффлайн-валидатор HTML
+
+		const report = validateInstance.validateString(htmlCode, htmlvalidateOptions);
+
+		report.results.forEach(({ messages }) => {
+			messages.forEach(({ column, line, message, selector, severity }) => {
+				if (Severity[severity]) {
+					const { logOutput, title } = Severity[severity];
+					const prefix = `\n[${chalk.cyan('Validate HTML Offline')}] ${sourceStr}(${line}:${column})`;
+					const selectorMsg = selector ? ` ${chalk.cyan(selector)}` : '';
+
+					output += `${prefix}${selectorMsg}:\n${logOutput.underline(title)}: ${logOutput(message)}\n`;
+				}
+			});
+		});
+	}
+
+	return output;
+};
+
+module.exports = {
+	validateHtml,
+	getPosthtmlW3c: ({
+		getSourceName = (tree) => tree.options.from,
+		log = console.log,
+		htmlvalidateOptions = DEFAULT_OPTIONS
+	} = {}) => async (tree) => {
+		const validationMessage = await validateHtml({
+			htmlCode: render(tree),
+			sourceName: getSourceName(tree),
+			htmlvalidateOptions
+		});
+
+		if (validationMessage) {
+			log(validationMessage);
+		}
+
+		return tree;
+	}
+};
